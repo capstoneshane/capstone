f1e5085a4cd1c4c39ca83e40f3eb1c2f652c3f0d
jpshane <jpshane@Jordans-MacBook-Air-3.local>
2016-06-28 01:46:09 -0400

    Twitter
    Header and Twitter feed

---
+++ footer.inc.php
@@ -0,0 +1,13 @@
+<div class="container">
+<div class="row">
+	<h3> Welcome to Sky Restaurant, where our delicious food will put you on Cloud 9! </h3>
+
+
+	<a class="twitter-timeline"  href="https://twitter.com/capstone_shane" data-widget-id="735169791070244864" data-tweet-limit="4">Tweets by @capstone_shane</a>
+	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+	          
+</div>
+</div>
+
+</body>
+</html>
